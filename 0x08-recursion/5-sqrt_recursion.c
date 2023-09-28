42d46df3ffbaaa684511b2fb1f551615d2a78714
Dimma2798 <chidinma.maxwell.191772@unn.edu.ng>
2023-09-28 14:14:41 -0700

    0x08-recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,42 +1,33 @@
 #include "main.h"
 
 /**
- * sqrt_find - Helper function to find the square root using binary search.
- * @low: The lower bound of the search range.
- * @high: The upper bound of the search range.
- * @c: The number to find the square root of.
+ * sqrt_find - first checks for the square root of given number
+ * @g: the root to check through
+ * @c: number to find sqrt of
  *
- * Return: The square root of c, or -1 if it does not have a natural square root.
+ * Return: The sqrt of C or -1 if number does not have natural sqrt
  */
-int sqrt_find(int low, int high, int c)
+
+int sqrt_find(int g, int c)
 {
-    if (low > high)
-        return (-1);
-
-    int mid = low + (high - low) / 2;
-    long long square = (long long)mid * mid;
-
-    if (square == c)
-        return mid;
-    else if (square < c)
-        return sqrt_find(mid + 1, high, c);
-    else
-        return sqrt_find(low, mid - 1, c);
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_find(g + 1, c));
 }
 
 /**
- * _sqrt_recursion - Returns the natural square root of a number.
- * @n: The number to find the square root of.
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: number to find sqrt of
  *
- * Return: The natural square root of n, or -1 if n is negative or has no natural square root.
+ * Return: natural square root or -1(if not natural square root)
  */
+
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-        return (-1);
-    if (n == 0)
-        return (0);
-
-    return sqrt_find(1, n, n);
+	if (n == 0)
+		return (0);
+	return (sqrt_find(1, n));
 }
 
